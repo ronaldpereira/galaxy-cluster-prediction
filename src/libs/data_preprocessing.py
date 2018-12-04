a321e4a4cb3ca904817516529c9d22152fdbea39
Ronald <ronald.drp11@gmail.com>
2018-12-04 10:53:35 -0200

    class balance implementation

--- src/libs/data_preprocessing.py
+++ src/libs/data_preprocessing.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.preprocessing import LabelBinarizer
 
 class DataPreprocessing:
@@ -8,3 +9,15 @@ class DataPreprocessing:
         class_encoded = self.lb.fit_transform(class_col)
 
         return class_encoded
+
+    def balance_class(self, dataframe, class_to_balance, multiplier):
+        class_df = dataframe.loc[dataframe['class'] == class_to_balance].reset_index().copy()
+
+        concat_array = [dataframe]
+
+        for _ in range(multiplier-1):
+            concat_array.append(class_df)
+
+        dataframe = pd.concat(concat_array, ignore_index=True)
+
+        return dataframe
--- src/sdss_prediction.py
+++ src/sdss_prediction.py
@@ -14,6 +14,9 @@ sdss = pd.read_csv(args.input_path)
 
 data_prep = DP.DataPreprocessing()
 
+# Uncomment the line below to balance QSO class
+# sdss = data_prep.balance_class(sdss, 'QSO', 6)
+
 encoded_classes = data_prep.class_encoder(sdss['class'])
 
 x_train, x_test, y_train, y_test = train_test_split(sdss.drop('class', axis=1).values, encoded_classes, train_size=0.666)
